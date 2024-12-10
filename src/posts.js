ec3455d089df25d23939805b723a4caa8f01432f
thebardofavon <152669825+thebardofavon@users.noreply.github.com>
2024-12-10 14:42:05 +0530

    Update

--- src/posts.js
+++
@@ -1,9 +0,0 @@
-export const posts = [
-  {
-    id: 1,
-    title: 'Post One',
-    content: 'This is the content of the first post.',
-    date: '2024-12-01',
-    image: 'https://www.gardendesign.com/pictures/images/675x529Max/site_3/ring-of-fire-sunflower-bicolor-sunflower-all-america-selections_12080.jpg', // Replace with your image URL
-  },
-];
