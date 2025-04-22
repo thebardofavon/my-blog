cb57246f0652712e3534f6eabb99103d7fc4f443
thebardofavon <152669825+thebardofavon@users.noreply.github.com>
2025-04-22 14:00:38 +0530

    Add AnalyticsTracker component for Google Analytics integration

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import AnalyticsTracker from './components/AnalyticsTracker'; 
 
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col">
+      <AnalyticsTracker /> 
       <Navbar />
       <main className="flex-grow container mx-auto p-4">
         <Outlet />
